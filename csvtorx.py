84c30b4dd816d54e7022f53fe79e39d7540ec11d
crackerjacques <129548912+crackerjacques@users.noreply.github.com>
2023-04-07 07:16:08 +0900

    Update csvtorx.py

--- csvtorx.py
+++ csvtorx.py
@@ -2,7 +2,7 @@ import argparse
 from decimal import Decimal
 
 def convert_csv_to_text(input_file, output_file):
-    with open(input_file, 'r') as f:
+    with open(input_file, 'r', encoding="utf-8") as f:
         lines = f.readlines()
 
     converted_lines = ['Marker file version: 1\n', 'Time format: Time\n']
@@ -16,7 +16,7 @@ def convert_csv_to_text(input_file, output_file):
             else:
                 converted_lines.append(f'{name}\t{start}\n')
 
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding="utf-8") as f:
         f.writelines(converted_lines)
 
 def main():
